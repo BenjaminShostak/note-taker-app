8fb35ec7c683818c7c950494098348899f39de72
Benjamin Shostak <benshostak@rocketmail.com>
2020-12-06 23:08:12 -0800

    server running

---
+++ server.js
@@ -0,0 +1,52 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var http =require("http");
+var fs = require("fs");
+
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function handleRequest(request, response) {
+
+    // Send the below string to the client when the user visits the PORT URL
+    response.end("It Works!! Path Hit: " + request.url);
+  }
+  
+  // Use the Node HTTP package to create our server.
+  // Pass the handleRequest function to empower it with functionality.
+  var server = http.createServer(handleRequest);
+  
+  // Start our server so that it can begin listening to client requests.
+  server.listen(PORT, function() {
+  
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+    app.end()
+
+
+    app.post("/", function(req, res) {
+        res.send("");
+      });
+      
+      
+
+    return fs.readFile(__dirname + "/index.html", function(err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+}
+
+  });
+  
